5c7c2e7cc4a002b67f70bcb19eff90e8c758a36b
Burak <burakozogul@hotmail.com>
2021-10-18 23:41:01 +0300

    First commit

---
+++ .py
@@ -0,0 +1,38 @@
+# x = 5
+# y = 4
+# z = 20
+
+""" x, y, z, = 5, 10, 20
+
+
+x += 5 # x= x + 5
+x *= 5 
+x /= 5
+
+print(x, y, z) """
+
+""" x, y, z, = 3, 5, 8
+
+a= int(input('1.Sayı:'))
+b= int(input('2.Sayı:'))
+
+result = (a*b) - (x+y+z)
+print(result) """
+
+""" x, y, z, = 3, 5, 8
+
+result = (2-y) // (x)
+print(result) """
+
+
+
+""" x, y, z, = 3, 5, 8
+
+sum = (x + y + z)
+Result = (sum) % (3)
+print(Result) """
+
+x, y, z, = 3, 5, 8
+
+result = (y) ** (x)
+print(result)
